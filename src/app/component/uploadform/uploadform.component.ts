5643dd851a54a32f6bcb4bcf97fbea4c331098da
al-uminium <aeronafic@gmail.com>
2024-06-11 21:41:42 +0800

    finished basic upload service and upload form functionalities

--- src/app/component/uploadform/uploadform.component.ts
+++ src/app/component/uploadform/uploadform.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UploadService } from '../../service/upload.service';
 
 @Component({
   selector: 'app-uploadform',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './uploadform.component.html',
   styleUrl: './uploadform.component.css'
 })
-export class UploadformComponent {
 
+export class UploadformComponent implements OnInit {
+  fb!: FormGroup
+  file!: File
+
+  private readonly uploadSvc = inject(UploadService);
+
+  ngOnInit(): void {
+    this.fb = new FormGroup({
+      file: new FormControl("", Validators.required)
+    })
+  }
+
+  onFileSelected(event: any): void {
+    this.file = event.target.files[0];
+  }
+
+  onSubmit(): void {
+    this.uploadSvc.convertToBlob(this.file).then(
+      (blob) => {
+        this.uploadSvc.fileUpload(blob, this.file.name)
+      }
+    )
+  }
 }
--- src/app/service/upload.service.ts
+++ src/app/service/upload.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
+  private readonly http = inject(HttpClient);
+  private readonly url: string = "http://localhost:8080/api/upload"
+
+  convertToBlob(file: File): Promise<Blob> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = () => {
+        resolve(new Blob([reader.result!], { type: file.type}));
+      }
+      reader.onerror = (e) => {
+        reject(e);
+      }
+    })
+  }
+
+  fileUpload(blob: Blob, fileName: string): Subscription {
+    const formData = new FormData();
+    formData.append('image', blob, fileName);
+    return this.http.post(this.url, formData).subscribe({
+      complete: () => console.info("Completed"),
+      error: (e) => console.error(e)
+    })
+  }
 
   constructor() { }
 }
