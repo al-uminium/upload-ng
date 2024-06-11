503a611fea0cf439e79017fa7d05ce3862365d63
al-uminium <aeronafic@gmail.com>
2024-06-11 17:58:23 +0800

    initialized upload app for practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UploadformComponent } from './component/uploadform/uploadform.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UploadformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/component/uploadform/uploadform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uploadform',
+  standalone: true,
+  imports: [],
+  templateUrl: './uploadform.component.html',
+  styleUrl: './uploadform.component.css'
+})
+export class UploadformComponent {
+
+}
